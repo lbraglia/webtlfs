2c4111a9490cf9318e5ee8ae8fea4e45b190eff6
Luca Braglia <lbraglia@gmail.com>
2024-10-11 17:34:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# webxlcrf
+
+Basically [xlcrf](https://pypi.org/project/xlcrf/), but repacked as an
+online streamlit service.
+
+The app is available [here](https://share.streamlit.io/lbraglia/webxlcrf/main).
+
---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+from xlcrf.CRF import CRF
+import tempfile
+import streamlit as st
+
+# File uploader
+xlsx = st.file_uploader(label = "Fai l'upload del file struttura qui, dopodiché clicca su 'Download CRF'.",
+                        type = ["xlsx"],
+                        accept_multiple_files = False)
+
+if xlsx is not None:
+    struc = tempfile.NamedTemporaryFile(suffix = '.xlsx')
+    out = tempfile.NamedTemporaryFile(suffix = '.xlsx')
+    strucfile = struc.name
+    outfile = out.name
+    # salvo per comodità il file in un file
+    with open(strucfile, "wb") as f:
+        f.write(xlsx.getbuffer())
+    crf = CRF()
+    crf.read_structure(strucfile)
+    crf.create(outfile)
+    with open(outfile, "rb") as f:
+        btn = st.download_button(
+            label = "Download CRF",
+            data = f,
+            file_name = "crf.xlsx")
+    
+# Include README
+with open('frontpage.md', 'r') as fp:
+    frontpage = fp.read()
+st.markdown(frontpage)
+        
